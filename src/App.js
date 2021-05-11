c6564fa1bf690f8482d8c4dc4f6f1730b1dd4a41
MuhammadShahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2021-05-11 10:14:26 +0600

    installing-and-setup-dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+const engine = new Styletron();
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
 
 function App() {
   return (
-    <div className="app">
-      <h1>In The name of ALLAH</h1>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Centered>
+          <div className="app">
+            <h1>In The name of ALLAH</h1>
+          </div>
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
+    
   );
 }
 
